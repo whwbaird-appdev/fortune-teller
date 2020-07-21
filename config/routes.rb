d0ef0e57b20c6b081030cedbac480a9f3fde3136
Raghu Betina <raghu@firstdraft.com>
2020-07-21 13:57:05 -0500

    better nav

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
   # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
 
-  # get("/zodiacs/aries", { :controller =>  fire, action =>  "ram" })
+  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
   # get ("/zodiacs/leo", { :controller => "fire", :action => "lion" })
   # get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
