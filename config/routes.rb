abbda29968889317413bf47871f24e6ad291a274
whw-baird <wbaird@chicagobooth.edu>
2021-02-01 15:57:30 +0000

    Fixed Capricorn

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   
   get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
   get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
   # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
   # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
